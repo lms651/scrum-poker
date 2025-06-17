6f0c0a56e69d8a82e2db49b2ae9af0e32895c8e9
lms651 <lmschmid@bu.edu>
2025-06-16 22:20:35 -0400

    Added Welcome Page & Product Owner Setup Page

---
+++ app.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const startButeton = document.getElementById("start-button")
+    startButeton.addEventListener('click', function() {
+    window.location.href = 'startgame.html';
+  });
+})
